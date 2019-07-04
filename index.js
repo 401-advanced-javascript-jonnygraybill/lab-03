1b578700d28716f13855a4b165a65100b01afc4f
Jonny Graybill <jon.a.graybill@gmail.com>
2019-07-04 14:04:34 -0700

    just need to refactor tests for promises

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const fileReader = require('./lib/reader.js');
+const fileReader = require('./lib/reader-promises.js');
+
 
 // Obtain and assert input
-// let files = process.argv.slice(2);
 let files = [`${__dirname}/files/1.txt`, `${__dirname}/files/2.txt`, `${__dirname}/files/3.txt`]
 
 if( ! (files instanceof Array && files.length) ) {
